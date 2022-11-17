16ea31a6978fc2b65d07ffc7c98c323ad28bcaf6
bihlink <95501288+bihlink@users.noreply.github.com>
2022-11-17 23:14:02 +0100

    Update inhead.php

--- inc/inhead.php
+++ inc/inhead.php
@@ -1,7 +1,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge,chrome=1">
-    <link rel="stylesheet" href="https://raw.githubusercontent.com/bihlink/php-vercel-directory/main/inc/style.css">
+    <link rel="stylesheet" href="https://bihlink.com/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
@@ -9,4 +9,4 @@
     <link rel="icon" href="<?= $rooturl; ?>/img/icon.png" sizes="32x32" />
     <link rel="icon" href="<?= $rooturl; ?>/img/icon.png" sizes="192x192" />
     <link rel="apple-touch-icon" href="<?= $rooturl; ?>/img/icon.png" />
-    <script src="https://raw.githubusercontent.com/bihlink/php-vercel-directory/main/inc/lightswitch.js"/></script>
+    <script src="https://bihlink.com/inc/lightswitch.js"/></script>
