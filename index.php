c5642b51c2fd699ac8242c39e94c8cda53ea8255
bihlink <95501288+bihlink@users.noreply.github.com>
2022-11-18 19:26:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -32,46 +32,51 @@ require_once('inc/config.php');
 <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="bi bi-arrow-up-circle"></i></button>
 <?php include('inc/navbar.php'); ?>
     <div class="container">
-      <h1>Fedirectory</h1>
 
-	<!-- // Trending Videos -->
-        <div class="row">
-            <div class="col-12"><h2><i class="bi bi-play-btn"></i> U Trendu</h2></div>
-            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-3">
-                <?php
-                $apidata = "https://invidious.sethforprivacy.com/api/v1/trending/?region=BA&pretty=1";
-                $api = file_get_contents($apidata);
-                $arr = json_decode($api, true);
-
-                foreach(array_slice($arr, 0, 40) as $data) {
-                $vidid = $data['videoId'];
-                $title = $data['title'];
-                $channelId = $data['authorId'];
-                $channelTitle = $data["author"];
-                $duzina = $data['lengthSeconds'];
-                    if($duzina >= '3600') {
-                        $time = gmdate("H:i:s", $duzina);
-                    } else {
-                        $time = gmdate("i:s", $duzina);
-                    }
-                ?>
-                  <div class="col">
-                    <div class="card position-relative">
-                      <a href="https://bihlink.com/watch/?v=<?= $vidid; ?>"><img src="https://img.youtube.com/vi/<?= $vidid; ?>/mqdefault.jpg" alt="<?= $title; ?>" style="width:100%;" class="card-img-top"></a>
-                      <span class="position-absolute top-0 end-0 px-1 bg-danger m-1"><?= $time; ?></span>
-                      <div class="card-body">
-                        <a href="https://bihlink.com/watch/?v=<?= $vidid; ?>" title="<?= $title; ?>">
-                            <h6 class="box-post-title"><?= $title; ?></h6>
-                        </a>
-                        <p class="card-text"><i class="bi bi-person-circle smicon"></i> <a href="https://bihlink.com/channel/?id=<?= $channelId; ?>"><?= $channelTitle; ?></a></p>
-                      </div>
-                    </div>
-                  </div>
+                <div class="row">
+                    <div class="col-12"><h2><i class="bi bi-play-btn"></i> <?= $seotitle; ?></h2></div>
+                    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-3">
                         <?php
-                    }
-                ?>
-            </div>
-        </div><!-- row 1 -->
+                        $apidata = "https://fedicard.bihlink.com/data/cardsdata.json";
+                        $api = file_get_contents($apidata);
+                        $arr = json_decode($api, true);
+        
+                        foreach(array_slice($arr, 0, 40) as $data) {
+                        $name = $data['name'];
+                        $username = $data['username'];
+                        $avatar = $data['avatar'];
+                        $cardimg = $data['cardimg'];
+                        $description = $data["description"];
+                        $description = $data["description"];
+                        $type = $data["type"];
+                        $instanceurl = $data["instanceurl"];
+                        $replies = $data["replies"];
+                        $boosts = $data["boosts"];
+                        $indirectory = $data["indirectory"];
+                        $approved = $data["approved"];
+                        $featured = $data["featured"];
+                        $cid = $data["cid"];
+                        ?>
+                          <div class="col">
+                            <div class="card position-relative">
+                              <a href="<?= $instanceurl; ?>/@<?= $username; ?>" target="_blank"><img src="<?= $cardimg; ?>" style="width:100%;" class="card-img-top"></a>
+                              <div class="card-body">
+                                <a href="<?= $instanceurl; ?>/@<?= $username; ?>" target="_blank">
+                                    <h6 class="box-post-title"><?= $name; ?></h6>
+                                </a>
+                                <p class="card-text"><a href="<?= $instanceurl; ?>/@<?= $username; ?>" target="_blank">@<?= $username; ?></a> / <a href="https://fedicard.bihlink.com/admin/viewcard.php?view=<?= $cid; ?>" target="_blank">View Card</a></p>
+                                <p class="card-text"><?= $description; ?></p>
+                                <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteCatModal<?= $cid; ?>">Delete</button>
+                                <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#editCatModal<?= $cid; ?>">Edit</button>
+                              </div>
+                            </div>
+                          </div>
+			<?php
+                            }
+                        ?>
+                    </div>
+                </div><!-- row 1 --> 
+
     </div><!-- container -->
 <?php include('inc/footer.php'); ?>
 </body>
