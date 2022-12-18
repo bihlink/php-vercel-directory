58100bac01d6a0a921c8fd33cac178bc276b5736
bihlink <95501288+bihlink@users.noreply.github.com>
2022-12-18 23:01:30 +0100

    Update index.php

--- card/index.php
+++ card/index.php
@@ -22,6 +22,7 @@ $card = array_search($view, array_column( $json, 'cid' ) );
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://maplefeed.bihlink.com/modern-light.css"></link>
     <base target="_blank">
     <meta itemprop="name" content="<?php if($json[$card]["name"] != "") {echo $json[$card]["name"];} else { echo "FediCard"; } ?>">
     <meta itemprop="description" content="<?php echo $json[$card]["description"]; ?>">
