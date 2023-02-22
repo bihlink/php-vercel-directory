a85be2d643b12034de85dc5e7e4255e729e0caa7
bihlink <95501288+bihlink@users.noreply.github.com>
2023-02-22 20:03:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -54,6 +54,7 @@ require_once('inc/config.php');
 	    $matches = str_replace('/tags/', 'https://fedi.bihlink.com/tags/', $matches);
             echo "<table style='width:100%;'>$matches[0]</table>";
           ?>
+	  <iframe allowfullscreen sandbox="allow-top-navigation allow-scripts" width="100%" height="1000" src="https://nitter.bihlink.com/abidnev"></iframe>
         </div>
       </div>
     </div><!-- container -->
